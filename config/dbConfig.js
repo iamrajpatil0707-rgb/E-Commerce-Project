5335f3bc438cc37af885d454d91a91eb0f0ed9f8
raj_patil <iamrajpatil0707@gmil.com>
2026-02-18 13:57:25 +0530

    removing comments

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -13,20 +13,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
-
-
-// const mongoose = require('mongoose');
-// require('dotenv').config();
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-//     console.log("MongoDB connected..."); // Ye message terminal mein aayega agar connection successful ho
-//   } catch (err) {
-//     console.error(err);
-//     process.exit(1); // Agar connection mein koi error aata hai toh server ko shutdown kar dega
-//   }
-// };
-
-// module.exports = connectDB;
